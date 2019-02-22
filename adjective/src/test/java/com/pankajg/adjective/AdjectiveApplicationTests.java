849a901cf9441e0e7c1c731355c286a4e8dce2e3
PankajG <pankaj.gurumukhi@talentica.com>
2019-02-23 00:48:32 +0530

    working service for adjective

---
+++ adjective/src/test/java/com/pankajg/adjective/AdjectiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pankajg.adjective;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdjectiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
